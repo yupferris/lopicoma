64967453a00949f25d25250b14779b373cd54460
ferris <yupferris@gmail.com>
2021-07-16 19:37:10 +0200

    Hook up PIO test
    Things are scaling perfectly and timing looks great. I can easily get up to >60mhz pulses, which is far beyond what we actually need. It should be relatively easy to get a DAC working at some useful multiple of the PAL color carrier frequency, assuming we have relatively fine control of the overall clock frequency.

--- lopicoma.c
+++ lopicoma.c
@@ -1,4 +1,7 @@
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
+
+#include "pulse.pio.h"
 
 int main()
 {
@@ -6,19 +9,18 @@ int main()
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
 
-    gpio_put(led_pin, false);
-
     const uint pulse_pin = 2;
-    gpio_init(pulse_pin);
-    gpio_set_dir(pulse_pin, GPIO_OUT);
 
-    gpio_put(led_pin, true);
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &pulse_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pulse_program_init(pio, sm, offset, pulse_pin);
 
     while (true)
     {
-        gpio_put(pulse_pin, true);
-        sleep_us(3);
-        gpio_put(pulse_pin, false);
-        sleep_us(3);
+        gpio_put(led_pin, true);
+        sleep_ms(250);
+        gpio_put(led_pin, false);
+        sleep_ms(250);
     }
 }
