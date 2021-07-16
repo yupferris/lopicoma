1c34490355e27b31546dc5509b367d8eab181c03
ferris <yupferris@gmail.com>
2021-07-16 15:40:34 +0200

    Verify basic GPIO
    Verified both the period (2ms) and the voltage (3.3V) on the scope.

--- lopicoma.c
+++ lopicoma.c
@@ -6,11 +6,19 @@ int main()
     gpio_init(led_pin);
     gpio_set_dir(led_pin, GPIO_OUT);
 
+    gpio_put(led_pin, false);
+
+    const uint pulse_pin = 2;
+    gpio_init(pulse_pin);
+    gpio_set_dir(pulse_pin, GPIO_OUT);
+
+    gpio_put(led_pin, true);
+
     while (true)
     {
-        gpio_put(led_pin, true);
-        sleep_ms(250);
-        gpio_put(led_pin, false);
-        sleep_ms(250);
+        gpio_put(pulse_pin, true);
+        sleep_ms(1);
+        gpio_put(pulse_pin, false);
+        sleep_ms(1);
     }
 }
